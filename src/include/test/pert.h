abf1e1b79edb11afa04970952f43034e152504d9
Johann Fotsing <johann.fotsing@outlook.com>
2024-09-11 12:17:03 +0200

    Group test functions in a header file in include folder.

---
+++ src/include/test/pert.h
@@ -0,0 +1,26 @@
+/***
+ * @brief
+ * @author Johann Fotsing
+ * @date 2024-09-11
+ * @file pert.h
+ */
+
+#include <iostream>
+#include <pert.h>
+#include <fstream>
+#include <sstream>
+#include <streambuf>
+#include "../depends/rapidxml-1.13/rapidxml.hpp"
+
+using namespace pert;
+using namespace rapidxml;
+
+template class activity_t<int>;
+template class network_t<int, int>;
+
+using network = network_t<int, int>;
+
+// Test functions
+int test_basic(const network&);
+int test_from_dummy();
+int test_from_txt(const char*);
--- src/test/pert.cpp
+++ src/test/pert.cpp
@@ -6,29 +6,11 @@
  */
 
 #include <iostream>
-#include <pert.h>
+#include "../include/test/pert.h"
 #include <fstream>
 #include <sstream>
 #include <streambuf>
 #include "../depends/rapidxml-1.13/rapidxml.hpp"
-
-using namespace pert;
-using namespace rapidxml;
-
-template class activity_t<int>;
-template class network_t<int, int>;
-
-using network = network_t<int, int>;
-
-// Test functions
-int test_basic(const network&);
-int test_from_dummy();
-int test_from_txt(const char*);
-
-int main(int argc, char** argv)
-{   
-    return test_from_txt(argv[1]);
-}
 
 int test_from_dummy()
 {
